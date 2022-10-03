5a50641532e99715cc17404d496cb4043e7b59c4
Min Maung <minmaung0307@gmail.com>
2022-10-03 16:02:03 -0400

    set up server

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
-const path = require("path");
 const express = require("express");
-const exphbs = require("express-handlebars");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, Ko Min Maung</h1>");
+});
 
-const hbs = exphbs.create({});
+const PORT = process.env.PORT || 3005;
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(require("./controllers/"));
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
 });
